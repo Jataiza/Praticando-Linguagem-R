202f8e59414da0e34e3a8cce31068643fceaf7d0
Jataiza <85134137+Jataiza@users.noreply.github.com>
2025-03-18 11:47:54 -0300

    Aprendendo R - Prática01

---
+++ Prática_01.R
@@ -0,0 +1,48 @@
+
+
+#RESPOSTA DO EXERCICIO DE FIXAÇÃO DE R - PRÁTICA (LIVRO UTILIZANDO A LINGUAGEM  ) 
+
+#[1] Crie três vetores X,Y, E Z e uma matriz quadrada com esses vetores em coluna
+x=c(1,2,2)
+y=c(1/2,1,1)
+z=c(1/2,1,1)
+
+#Criando matriz de coluna 
+
+matriz<-cbind(x, y, z)
+print(matriz)
+
+#Criando matriz de linha
+
+matriz<- rbind(x,y,z)
+print(matriz)
+
+#[2] Calcule a média e a mediana de X
+
+#Media de X
+result.mean <- mean(x)
+print(result.mean)
+
+#Mediana de Z
+median.result <- median(z)
+print(median.result)
+
+#[3] Calcule a correlação entre x e seu vetor de soma acumulada
+
+#[4] Obtenha as dimensões da matriz M
+
+#[5] Obtenha os autovalores e autovetores da matriz M
+
+#[6] Obtenha o piso e o teto do valor 8.799
+
+#[7] Arredonde o valor 8.799 para uma casa decimal 
+
+#[8] Encontre as raízes dp polinômio x-9 retornado os valores
+
+#[9] Retorne as diferenças entre os elementos consecutivos do vetor x
+
+#[10] Obtenha o vetor de somas acumuladas do vetor z, o vetor do produto acumus
+
+
+
+
